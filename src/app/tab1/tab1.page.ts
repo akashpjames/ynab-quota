affbd1c50271351c0d42f2d4c54744c1fdaea07a
Akash P James <akashpjames@gmail.com>
2019-09-09 12:34:21 +0530

    Minor bug fix and cleaning up of some code

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -18,7 +18,7 @@ export class Tab1Page {
     templateToUse = '';
     templates: any = [];
     expense: any = false;
-    public showbuttons: any;
+    public showbuttons: boolean;
     private headers: { Authorization: string };
     public totalSyncs: number;
     quotaData: any = [];
@@ -51,7 +51,6 @@ export class Tab1Page {
                     'Authorization': `Bearer ${val}`
                 };
                 this.getQuotas();
-                // this.getFakeData();
             } else {
                 this.commonService.createToast('Access Token not available', 'dark');
             }
@@ -173,13 +172,14 @@ export class Tab1Page {
                         console.log(val);
                         var subcategoriesList = this.budgetData.filter(x => x.id === val.cat_id);
                         var categoryInformation = subcategoriesList[0].categories.filter(y => y.id === val.sub_cat_id);
-                        if (categoryInformation[0].budgeted > 0) categoryInformation[0].width = (categoryInformation[0].balance * 100 / categoryInformation[0].budgeted) + '%';
+                        if (categoryInformation[0].budgeted > 0) {
+                            let value = categoryInformation[0].balance * 100 / categoryInformation[0].budgeted;
+                            value = value < 0 ? 0 : value;
+                            categoryInformation[0].width = `${value}%`;
+                        }
                         categoryInformation[0].balance_edited = this.updateCurrency(categoryInformation[0].balance);
                         categoryInformation[0].budgeted_edited = this.updateCurrency(categoryInformation[0].budgeted);
                         this.quotaData.push(categoryInformation[0]);
-                        if (categoryInformation[0].budgeted < 1) {
-
-                        }
                     })
                     this.intialLoading = false;
                     loading.dismiss();
@@ -223,8 +223,6 @@ export class Tab1Page {
         }
     }
 
-
-
     clearTemplates() {
         this.storage.clear().then(() => {
             this.commonService.createToast('All templates have been cleared', 'dark');
@@ -247,178 +245,5 @@ export class Tab1Page {
 
     deleteKey(data) {
         this.storage.remove(data.key);
-    }
-
-    updateLastTime() {
-        const date = new Date();
-        date.setDate(date.getDate() - 2);
-        this.storage.set('lastUpdate', date);
-    }
-
-    getFakeData() {
-        this.quotaData = [
-            {
-                "id": "d9587eef-8837-494b-a527-5d5c0f941dd0",
-                "category_group_id": "14af761d-b74c-4062-8a92-693233b3a066",
-                "name": "Water",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": "₹1,234.00",
-                "activity": -140000,
-                "balance": "₹1,219.22",
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "86%"
-            },
-            {
-                "id": "da18294f-16ce-4d12-9156-fb2b64a9ad7a",
-                "category_group_id": "4a787888-7b9f-491e-acd3-a411b87c8d18",
-                "name": "Eating Out",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": 10000,
-                "activity": -3000,
-                "balance": 7000,
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "70%"
-            },
-            {
-                "id": "da18294f-16ce-4d12-9156-fb2b64a9ad7b",
-                "category_group_id": "4a787888-7b9f-491e-acd3-a411b87c8d18",
-                "name": "Fuel",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": 10000,
-                "activity": -3000,
-                "balance": 6000,
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "60%"
-            },
-            {
-                "id": "da18294f-16ce-4d12-9156-fb2b64a9ad7c",
-                "category_group_id": "4a787888-7b9f-491e-acd3-a411b87c8d18",
-                "name": "Movies",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": 10000,
-                "activity": -3000,
-                "balance": 5000,
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "50%"
-            },
-            {
-                "id": "da18294f-16ce-4d12-9156-fb2b64a9ad7d",
-                "category_group_id": "4a787888-7b9f-491e-acd3-a411b87c8d18",
-                "name": "Fun Money",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": 10000,
-                "activity": -3000,
-                "balance": 4000,
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "40%"
-            },
-            {
-                "id": "da18294f-16ce-4d12-9156-fb2b64a9ad7e",
-                "category_group_id": "4a787888-7b9f-491e-acd3-a411b87c8d18",
-                "name": "Gift",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": 10000,
-                "activity": -3000,
-                "balance": 3000,
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "30%"
-            },
-            {
-                "id": "da18294f-16ce-4d12-9156-fb2b64a9ad7f",
-                "category_group_id": "4a787888-7b9f-491e-acd3-a411b87c8d18",
-                "name": "Travel",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": 10000,
-                "activity": -3000,
-                "balance": 2000,
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "20%"
-            },
-            {
-                "id": "da18294f-16ce-4d12-9156-fb2b64a9ad7g",
-                "category_group_id": "4a787888-7b9f-491e-acd3-a411b87c8d18",
-                "name": "Shopping",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": 10000,
-                "activity": -3000,
-                "balance": 1000,
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "10%"
-            },
-            {
-                "id": "da18294f-16ce-4d12-9156-fb2b64a9ad7h",
-                "category_group_id": "4a787888-7b9f-491e-acd3-a411b87c8d18",
-                "name": "Groceries",
-                "hidden": false,
-                "original_category_group_id": null,
-                "note": null,
-                "budgeted": 10000,
-                "activity": -3000,
-                "balance": 500,
-                "goal_type": null,
-                "goal_creation_month": null,
-                "goal_target": 0,
-                "goal_target_month": null,
-                "goal_percentage_complete": null,
-                "deleted": false,
-                "width": "5%"
-            },
-        ]
     }
 }
